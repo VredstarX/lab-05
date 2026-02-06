4d85345e9be38c39b514f929ca25bf9421c43dbb
VredstarX <supermegalightnight@gmail.com>
2026-02-05 22:38:11 -0700

    Delete button

--- ListyCity/app/src/main/java/com/example/lab5_starter/CityDialogFragment.java
+++ ListyCity/app/src/main/java/com/example/lab5_starter/CityDialogFragment.java
@@ -62,20 +62,30 @@ public class CityDialogFragment extends DialogFragment {
             city = null;}
 
         AlertDialog.Builder builder = new AlertDialog.Builder(getContext());
-        return builder
+        builder
                 .setView(view)
                 .setTitle("City Details")
                 .setNegativeButton("Cancel", null)
                 .setPositiveButton("Continue", (dialog, which) -> {
                     String title = editMovieName.getText().toString();
                     String year = editMovieYear.getText().toString();
+
                     if (Objects.equals(tag, "City Details")) {
                         listener.updateCity(city, title, year);
                     } else {
                         listener.addCity(new City(title, year));
                     }
-                })
-                .create();
+                });
+
+        if (Objects.equals(tag, "City Details") && city != null) {
+            builder.setNeutralButton("Delete", (dialogInterface, whichButton) -> {
+                listener.deleteCity(city);
+            });
+        }
+
+        return builder.create();
+
     }
+
 
 }
